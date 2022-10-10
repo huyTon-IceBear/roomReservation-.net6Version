a65c9c5f87ba191c596eb88ccc861f6859f23e6f
Huy Ton <5497060-ramenbowl@users.noreply.gitlab.com>
2022-10-10 17:23:26 +0200

    Add project files.

---
+++ roomReservation/User.cs
@@ -0,0 +1,11 @@
+﻿namespace roomReservation
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+    }
+}
